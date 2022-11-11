474f04cc5006fd0448e8bfd39be0adf3cb58d63b
vishnuprakaash <vishnuprakaash.ravichandran@verticurl.com>
2022-11-11 13:12:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,114 @@
+
+var queue = [];
+        var input = 0;
+        function calculateQueue(value){
+            
+            var valnewarr=[];
+            for(var keyvall in value)
+            {
+
+                if(value[keyvall] == '(' || value[keyvall] == ')' || value[keyvall] == '0')
+                {
+                    
+                }else
+                {
+                    valnewarr.push(value[keyvall]);
+                }
+            }
+            value=valnewarr;
+            queue=valnewarr;
+           
+            if (input !==0) {
+            input = parseFloat(input);
+
+                addToQueue(input);
+                            }
+            var answer = value[0];
+            var dividedByZero = 0;
+            for (  var i = 2; i < value.length; i= i+2) {
+                switch (queue[i-1]) {
+                    case '+':
+                        answer+= value[i];
+                        break;
+                    case '-':
+                        answer-= value[i];
+                        break;
+						
+                    case '/':    if (value[i] === 0)   dividedByZero = 1;
+                            else      answer = answer / value[i];
+
+                        break;
+                    case'*': answer = answer * value[i];
+                        break;
+                    case')': answer = answer + value[i-1];
+                        break;
+                    
+						 
+						
+                }
+
+            }
+
+            answer = answer.toFixed(10);
+            answer = parseFloat(answer);
+            if ( dividedByZero === 1) { clearAll();
+                document.getElementById("answer").innerHTML =  "ERROR";}
+            else
+            {document.getElementById("answer").innerHTML =  answer ;
+                input = answer;
+            queue = [];}
+
+
+        }
+        function addToQueue(input){
+            queue.push(input);
+        }
+        function clearAll() {
+            queue = [];
+            input = 0;
+            document.getElementById("answer").innerHTML = "0";
+        }
+        function numericButton(arg){
+            if ( document.getElementById("answer").innerHTML ===  "ERROR" || (document.getElementById("answer").innerHTML == "0" && arg != "."))
+              { document.getElementById("answer").innerHTML = ""; }
+
+            if (!(arg === ".") || !input.match(/[.]/)) {
+
+            input += arg;
+            document.getElementById("answer").innerHTML += arg;}
+
+
+        }
+        function operatorButton(arg){
+              if (input !== 0 && input !== "-" ) {
+                  input = parseFloat(input);
+                  addToQueue(input);
+                  addToQueue(arg);
+                  document.getElementById("answer").innerHTML +=arg;
+                  input = 0;
+
+              }
+              if (arg == "-"  && isNaN(queue[0]) && input !== "-")
+              {input ="-";
+
+                  document.getElementById("answer").innerHTML = "-";
+
+
+            }
+
+
+        }
+
+        function bracktButton(arg){
+
+            if ( document.getElementById("answer").innerHTML ===  "ERROR" || (document.getElementById("answer").innerHTML == "0" && arg != "."))
+              { document.getElementById("answer").innerHTML = ""; }
+
+                  input = parseFloat(input);
+                  addToQueue(input);
+                  addToQueue(arg);
+                  document.getElementById("answer").innerHTML +=arg;
+                  input = 0;     
+
+
+        }
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path=require('path');
+module.exports={
+    entry:"./script.js",
+    output:{
+        filename:"bundle.js",
+        path:path.resolve(__dirname,"./dist")
+    },
+    mode:"none",
+    module:{
+        rules:[
+            {
+            test:/\.(png|jpg)$/,
+            use:['file-loader']
+        },
+        {
+           test:/\.css$/,
+           use:['style-loader','css-loader']
+        },
+        {
+            test:/\.scss$/,
+            use:['style-loader',
+            'css-loader',
+            {
+                loader:"sass-loader",
+                options:{
+                    implementation:require("SASS")
+                }
+            }
+        ]
+         }
+    ]
+       
+    }
+}
